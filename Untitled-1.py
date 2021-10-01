11bbd818b4137060c6d4f516d6fd1fe916591d33
shrutikuwar <89292362+shrutikuwar@users.noreply.github.com>
2021-10-02 00:56:12 +0530

    add id 5

--- Untitled-1.py
+++ Untitled-1.py
@@ -23,4 +23,10 @@
      "defination": "https://www.fingent.com/blog/cloud-service-models-saas-iaas-paas-choose-the-right-one-for-your-business/#PaaS",
      "examples": "https://www.fingent.com/blog/cloud-service-models-saas-iaas-paas-choose-the-right-one-for-your-business/#PaaS"
     }
+    {
+     "id": "5",
+     "model": "Private cloud",
+     "defination": "https://www.pluribusnetworks.com/products/adaptive-cloud-fabric/?_bt=541827214022&_bk=private%20cloud%20network&_bm=p&_bn=g&_bg=129331972231&gclid=CjwKCAjw49qKBhAoEiwAHQVToxkBIFrb_lNnIquNvo-GqQN492kAEsvpRjs-DPpU8KHFZyXYT68ELxoCF8gQAvD_BwE",
+     "examples": "https://in.searchley.com/ws?q=hpe%20hybrid%20cloud&asid=sl_in_ga_1_cg1_01&mt=b&nw=g&de=c&ap=&ac=16308&cid=14561692242&aid=127662577860&locale=en_IN&gclid=CjwKCAjw49qKBhAoEiwAHQVTo-pPxz7ebEPKDt9Dosv8yTi7eZo3djke82IofumVMHpeBbtqpfYPMxoCO7YQAvD_BwE"
+    }
 ]
